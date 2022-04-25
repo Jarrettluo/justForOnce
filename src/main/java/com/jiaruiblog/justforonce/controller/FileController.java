07716b006f1846b6283c8fa7fa56801d31483bde
luojiarui <luojiarui2@163.com>
2022-04-25 13:11:26 +0800

    增加了定时任务删除路径下文件夹和文件

--- src/main/java/com/jiaruiblog/justforonce/controller/FileController.java
+++ src/main/java/com/jiaruiblog/justforonce/controller/FileController.java
@@ -3,6 +3,7 @@ package com.jiaruiblog.justforonce.controller;
 import com.alibaba.fastjson.JSONObject;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +12,6 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
-import java.nio.charset.Charset;
 
 /**
  * @Author Jarrett Luo
@@ -25,7 +25,11 @@ public class FileController {
     @Value("${file.upload.url}")
     private String uploadFilePath;
 
-
+    /**
+     * 上传文件并保存文件到指定路径中
+     * @param files
+     * @return
+     */
     @SuppressWarnings("warning")
     @RequestMapping("/upload")
     public String httpUpload(@RequestParam("files") MultipartFile files[]){
@@ -51,6 +55,14 @@ public class FileController {
         return object.toString();
     }
 
+    /**
+     * 根据文件名下载指定文件
+     * @param request
+     * @param response
+     * @param fileName
+     * @return
+     * @throws UnsupportedEncodingException
+     */
     @RequestMapping("/download")
     public String fileDownLoad(HttpServletRequest request, HttpServletResponse response,
                                @RequestParam("fileName") String fileName) throws UnsupportedEncodingException {
@@ -62,7 +74,7 @@ public class FileController {
         // 获取要下载的文件名
         String formatFileName;
         String userAgent = request.getHeader("User-Agent");
-        // 针对ID或者以IE为内核的浏览器
+        // 针对IE或者以IE为内核的浏览器
         if(userAgent.contains("MSIE")||userAgent.contains("Trident")){
             formatFileName = java.net.URLEncoder.encode(fileName, "UTF-8");
         } else {
@@ -88,6 +100,38 @@ public class FileController {
             return "下载失败";
         }
         return "下载成功";
+    }
+
+    /**
+     * 定时任务，删除路径下所有文件夹和文件。
+     */
+    @Scheduled(cron="0 0 3 * * ?")
+    private void deleteFiles(){
+        deleteFile(new File(uploadFilePath));
+    }
+
+    public void deleteFile(File file){
+        //判断文件不为null或文件目录存在
+        if (file == null || !file.exists()){
+            log.info("暂无文件");
+            return;
+        }
+        //取得这个目录下的所有子文件对象
+        File[] files = file.listFiles();
+        //遍历该目录下的文件对象
+        for (File f: files){
+            //打印文件名
+            String name = f.getName();
+            log.info(name);
+            //判断子目录是否存在子目录,如果是文件则删除
+            if (f.isDirectory()){
+                deleteFile(f);
+            }else {
+                f.delete();
+            }
+        }
+        //删除空文件夹  for循环已经把上一层节点的目录清空。
+        file.delete();
     }
 
 }
